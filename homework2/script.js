ab23e61a8744602a80e84a15c4a1def4c149cef4
Vladyslav <vladislavsavcuk2@gmail.com>
2023-04-06 21:15:37 +0200

    add userInfo in script.js

---
+++ homework2/script.js
@@ -0,0 +1,18 @@
+ let userAge;
+ userAge = 18;
+ console.log(userAge);
+
+ let userName;
+ userName = 'Vlad';
+ console.log(userName);
+
+ let userSurname;
+ userSurname = 'Savchuk';
+ console.log(userSurname);
+
+ let space;
+ space = ' ';
+
+ const userInformation = userName + space + userSurname + space + userAge;
+ console.log(userInformation)
+
