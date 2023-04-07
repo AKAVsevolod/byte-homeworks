cc1c60ceeccd76b194f132e28d8aee3cbd65bc81
Vladyslav <vladislavsavcuk2@gmail.com>
2023-04-08 00:44:04 +0200

    added arithmetic operations to script.js in homework3

---
+++ homework3/script.js
@@ -0,0 +1,20 @@
+//1
+const a = 5;
+const b = 10;
+
+let additionAction;
+additionAction = a + b;
+console.log(`additionAction`, additionAction);
+
+let subtractionAction;
+subtractionAction = a - b;
+console.log(`subtractionAction`, subtractionAction);
+
+let multiplicationAction;
+multiplicationAction = a * b;
+console.log(`multiplicationAction`, multiplicationAction);
+
+let actionOfDivision;
+actionOfDivision = a / b;
+console.log(`actionOfDivision`, actionOfDivision);
+
