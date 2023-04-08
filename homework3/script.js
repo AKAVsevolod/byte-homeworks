9af73a4e609cde7437ca6fab42d747b175c3d61e
Vladyslav <vladislavsavcuk2@gmail.com>
2023-04-08 02:02:08 +0200

    checking the knowledge of operators in homework3

--- homework3/script.js
+++ homework3/script.js
@@ -18,3 +18,17 @@ let actionOfDivision;
 actionOfDivision = a / b;
 console.log(`actionOfDivision`, actionOfDivision);
 
+// 2
+console.log(true && true) // true
+console.log(false || true) // true
+console.log(true && 'false') // true не зрозумів
+console.log(0 && 1 || ' ' && 42) // 42
+console.log(null == undefined) // true
+console.log('' == 0) // true
+console.log(1 === '1') // false
+console.log(NaN === NaN && true) // false
+console.log(true || false && true || false); // true
+console.log((true || false) && (true || false)); // true
+console.log(10 || 0 && 'dog' || ''); // 10 
+console.log((10 || 0) && ('dog' || '')); // dog
+
