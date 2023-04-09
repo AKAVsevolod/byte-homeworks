6244717e29c17429d8767d4d5cd3ef89f3f4a94d
Vladyslav <vladislavsavcuk2@gmail.com>
2023-04-09 18:08:58 +0200

    finished task number1 in homework4

---
+++ homework4/script.js
@@ -0,0 +1,22 @@
+// 1
+//  function getNumbers() {
+//     let a = prompt('Number 1')
+//     let b = prompt('Number 2')
+
+//     let res = a == b; 
+
+//     console.log(res);
+    
+//  }
+//  getNumbers();
+
+// function getNumbers(a, b) {
+//     let res = a == b;
+//     console.log(res);
+// }
+
+// getNumbers(10, 10);
+
+
+
+
