1ee4bfd6a408b3c924ab044f6490a4e4b75a7ead
Vladyslav <vladislavsavcuk2@gmail.com>
2023-04-14 21:05:07 +0200

    connect script.js with index.html and finished task1 in homework5

---
+++ homework5/script.js
@@ -0,0 +1,40 @@
+//1
+
+// for  (let i = 1 ; i <= 10 ; i++) {
+//     console.log("i" + i);
+// };
+
+// let j = 1;
+
+// while (j <= 10) {
+//     console.log('j' + j);
+//     j++;
+// };
+
+// let a = 1;
+
+// do {
+//     console.log("a" + a);
+//     a++;
+// } while(a <= 10 )
+
+// for (let i = 20; i >= 10; i--) {
+//     console.log("i" + i);
+// }
+
+// let j = 20;
+
+// while (j >= 10) {
+//     console.log("j" + j)
+//     j--
+// }
+
+// let a = 20;
+
+// do {
+//     console.log('a' + a)
+//     a--;
+// } while (a >= 10);
+
+// 2
+
